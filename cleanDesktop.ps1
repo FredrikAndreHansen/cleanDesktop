b4dd14c2c4faab41bd95ad7cfbe177f0ec989f15
Fredrik Palladino Hansen <fredrikahansen90@gmail.com>
2025-04-06 21:22:33 +0200

    All functions complete

--- cleanDesktop.ps1
+++ cleanDesktop.ps1
@@ -1,16 +1,72 @@
 ﻿<# Initialize Variables #>
-[string]$desktopPath = "C:\Users\Fredrik\Desktop";
-[string]$customDesktopPath = "C:\Users\Fredrik\CustomDesktop\Files";
 
+<# Desktop path #>
+[string]$desktopPath = "C:\Users\Fredrik\Desktop";
+
+<# Destination paths #>
+[string]$customDesktopPath = "C:\Users\Fredrik\CustomDesktop\Files";
 [string]$imagesFolder = "\Images";
+[string]$documentsFolder = "\Documents";
+[string]$otherFolder = "\Other";
 
 [string]$imagesPath = $customDesktopPath + $imagesFolder;
+[string]$documentsPath = $customDesktopPath + $documentsFolder;
+[string]$otherPath = $customDesktopPath + $otherFolder;
 
 [string]$currentYear = Get-Date -f yyyy;
 [string]$currentMonth = Get-Date -f MMMM;
 [string]$destinationFolder = $currentMonth + "_" + $currentYear;
 
 [string]$fullImagePath = $imagesPath + "\" + $destinationFolder;
+[string]$fullDocumentPath = $documentsPath + "\" + $destinationFolder;
+[string]$fullOtherPath = $otherPath + "\" + $destinationFolder;
+
+[array]$imageExtensions = @(".png", ".jpg", ".gif", ".svg");
+[array]$documentExtensions = @(".txt", ".doc", ".docx", ".pdf", ".odt");
+
+<# Functions #>
+function Create-Folder-If-Not_exist {
+    param (
+        [string]$fullFilePath
+    )
+
+    [bool]$doesImageFolderExist = Test-Path -Path $fullFilePath;
+
+    if ($doesImageFolderExist -eq $false) {
+        mkdir -Path $fullFilePath;
+    }
+}
+
+function Move-File {
+    param (
+        $file, 
+        [string]$destinationPath
+    )
+
+    [string]$filePath = $desktopPath + "\" + $file.Name;
+
+    [string]$fileDestination = $destinationPath + "\" + $file.Name;
+    [bool]$doesFileExist = Test-Path -Path $fileDestination;
+
+    <# Move the file, if the name exist, then change the file name #>
+    if ($doesFileExist -eq $false) {
+        Move-Item -Path $filePath -Destination $destinationPath;
+    } else {
+        [int]$i = 0;
+        while($doesFileExist -eq $true) {
+            $i++;
+
+            $newFileName = $file.BaseName + $i.ToString() + $file.Extension;
+
+            $fileDestination = $destinationPath + "\" + $newFileName;
+            $doesFileExist = Test-Path -Path $fileDestination;
+        }
+
+        Rename-Item -Path $filePath -NewName $newFileName;
+        $filePath = $desktopPath + "\" + $newFileName;
+        Move-Item -Path $filePath -Destination $destinationPath;
+    }
+}
 
 <# Move files #>
 $getAllFiles = Get-ChildItem -Path $desktopPath;
@@ -18,16 +74,24 @@ $getAllFiles = Get-ChildItem -Path $desktopPath;
 forEach($file in $getAllFiles) {
 
     <# Images #>
-    if ($file.Extension -eq ".png" -or ".jpg" -or "gif" -or "svg") {
+    if ($file.Extension -in $imageExtensions) {
 
-        <# Check if folder exist, if not, then create it #>
-        [bool]$doesImageFolderExist = Test-Path -Path $fullImagePath;
-        if ($doesImageFolderExist -eq $false) {
-            mkdir -Path $fullImagePath;
-        }
+        Create-Folder-If-Not_exist -fullFilePath $fullImagePath;
 
-        <# Move image to folder #>
-        $filePath = $desktopPath + "\" + $file.Name;
-        Move-Item -Path $filePath -Destination $fullImagePath;
+        Move-File -file $file -destinationPath $fullImagePath;
+    } 
+    <# Documents #>
+    elseif ($file.Extension -in $documentExtensions) {
+
+        Create-Folder-If-Not_exist -fullFilePath $fullDocumentPath;
+
+        Move-File -file $file -destinationPath $fullDocumentPath;
+    } 
+    <# All other files #>
+    else {
+        
+        Create-Folder-If-Not_exist -fullFilePath $fullOtherPath;
+
+        Move-File -file $file -destinationPath $fullOtherPath;
     }
 }
